379b62ed057ca063d80cfd64f1a7f5c897c31b6f
Samuele Resca <samuele.resca@gmail.com>
2018-08-27 01:36:07 +0200

    Setup newsletter

---
+++ Controllers/NewsletterController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.SubscribeMeProject.Controllers
+{
+    [Route("api/newsletter")]
+    [ApiController]
+    public class NewsletterController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return NoContent();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
