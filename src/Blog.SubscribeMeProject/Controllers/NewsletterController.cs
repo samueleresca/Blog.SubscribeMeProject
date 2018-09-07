7a5d0aecbf62cb713b397e788455c90c6b705c4b
Samuele Resca <samuele.resca@just-eat.co.uk>
2018-09-07 10:27:18 +0100

    Added try catch to action methods

--- src/Blog.SubscribeMeProject/Controllers/NewsletterController.cs
+++ src/Blog.SubscribeMeProject/Controllers/NewsletterController.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Blog.SubscribeMeProject.Infrastructure.Models;
 using Blog.SubscribeMeProject.Infrastructure.Repositories;
 using Blog.SubscribeMeProject.Infrastructure.Requests;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Blog.SubscribeMeProject.Controllers
 {
@@ -12,52 +14,83 @@ namespace Blog.SubscribeMeProject.Controllers
     public class NewsletterController : ControllerBase
     {
         private readonly ISubscriptionRepository _subscriptionRepository;
+        private readonly ILogger<NewsletterController> _logger;
 
-        public NewsletterController(ISubscriptionRepository subscriptionRepository)
+        public NewsletterController(ISubscriptionRepository subscriptionRepository,
+            ILogger<NewsletterController> logger)
         {
             _subscriptionRepository = subscriptionRepository;
+            _logger = logger;
         }
 
         [HttpGet("{email:required}")]
         public async Task<IActionResult> Get([EmailAddress]string email)
         {
-            var subscription =  await _subscriptionRepository.Get(email);
 
-            if (subscription == null) return NotFound();
+            try
+            {
+                var subscription = await _subscriptionRepository.Get(email);
 
-            return Ok(subscription);
+                if (subscription == null) return NotFound();
+
+                return Ok(subscription);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return StatusCode(500);
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(SubscriptionRequest request)
         {
-            var result = await _subscriptionRepository.Get(request.Email);
+            try
+            {
 
-            if(result != null) {
-                return BadRequest();
+                var result = await _subscriptionRepository.Get(request.Email);
+
+                if (result != null)
+                {
+                    return BadRequest();
+                }
+
+                var subscription = new Subscription
+                {
+                    Email = request.Email,
+                    IsActive = true
+                };
+
+                await _subscriptionRepository.Add(subscription);
+
+                return Created(nameof(Get), new { email = subscription.Email });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return StatusCode(500);
             }
 
-            var subscription = new Subscription
-            {
-                Email = request.Email,
-                IsActive = true
-            };
-
-            await _subscriptionRepository.Add(subscription);
-
-            return Created(nameof(Get), new { email= subscription.Email });
         }
-        
+
         [HttpDelete("{email:required}")]
         public async Task<IActionResult> Delete([EmailAddress]string email)
         {
-            var target = await _subscriptionRepository.Get(email);
+            try
+            {
+                var target = await _subscriptionRepository.Get(email);
 
-            if (target == null) return NotFound();
-        
-            await _subscriptionRepository.Remove(target.Email);
+                if (target == null) return NotFound();
 
-            return NoContent();
+                await _subscriptionRepository.Remove(target.Email);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return StatusCode(500);
+            }
         }
 
     }
